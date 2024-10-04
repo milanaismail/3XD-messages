316a550783ff338473986aad2cae348e7ed21d03
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-10-04 12:29:29 +0200

    feat: add corse

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//cors
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,8 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors()); // cors toevoegen
+
 app.use('/api/v1/messages', require('./routes/api/v1/messages'));
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
