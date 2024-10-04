f5056124881aab51bcb336e5f5257f2151a7f098
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-10-04 11:57:01 +0200

    initial commit

---
+++ routes/api/v1/messages.js
@@ -0,0 +1,19 @@
+const express = require('express'); // om iets te requiren moet je exporten
+const router = express.Router(); // router functie
+
+// GET /api/v1/messages
+router.get('/', (req, res) => {
+  res.json({
+    status: 'success',
+    message: 'Hello, World!',
+    data: {
+      messages: [
+        { message: 'Hello, World!' },
+        { message: 'Hi, there!' },
+        { message: 'Goodbye, World!' }
+      ]
+    }
+  });
+});
+
+module.exports = router; // exporteren van router
