c8a37dc63fbfcb7c1bac8f0aef1959e588a564b2
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-10-04 15:04:29 +0200

    alles

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -3,21 +3,38 @@ const router = express.Router(); // router functie
 
 const app = express();
 app.use(express.json());
+
 let messages = [];
 
 // GET /api/v1/messages
 router.get('/', (req, res) => {
+  username = req.query.username;
+  if (req.query.username){
   res.status(200).json({
     status: 'success',
     message: 'Hello, World!',
     data: {
       messages: [
-        { message: 'Hello, World!' },
-        { message: 'Hi, there!' },
-        { message: 'Goodbye, World!' }
+        {
+          id: 1,
+          message: 'Hello, World!',
+          user : username
+        },
+        {
+          id: 2,
+          message: 'Hello, World!',
+          user : username
+        },
+        {
+          id: 3,
+          message: 'Hello, World!',
+          user : username
+        }
+      
       ]
     }
   });
+}
 });
 
 //get with id
@@ -25,22 +42,26 @@ router.get('/:id', (req, res) => {
   const id = req.params.id;
   res.status(200).json({
     status: 'success',
-    message: `Message ${id} updated!`,
+    message: `Message ${id} retrieved!`,
     data: {
       message: 'Hello, World!'
     }
   });
 });
 
+
+
 // POST /api/v1/messages 
 router.post('/', (req, res) => {
   const message = req.body.message;
+  const user = req.body.user;
   messages.push(message);
   res.status(201).json({
     status: 'success',
     message: 'Message added!',
     data: {
-      message: message
+      message: message,
+      user : user
     }
   });
 });
@@ -61,18 +82,6 @@ router.put('/:id', (req, res) => {
 }
 );
 
-// get with req.params.username
-router.get('/:username', (req, res) => {
-  const user = req.params.username;
-  res.status(200).json({
-    status: 'success',
-    message: `Message ${user} updated!`,
-    data: {
-      message: 'Hello, World!'
-    }
-  });
-});
-
 // DELETE /api/v1/messages/:id
 router.delete('/:id', (req, res) => {
   const id = req.params.id;
@@ -82,6 +91,5 @@ router.delete('/:id', (req, res) => {
     message: `Message ${id} deleted!`
   });
 });
-
 
 module.exports = router; // exporteren van router
